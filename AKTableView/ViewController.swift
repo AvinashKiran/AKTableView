ea716e758d6d8421977b68131bde7b237c858523
Avinash <avinashkiran001@gmail.com>
2018-06-18 23:08:46 +0530

    init

---
+++ AKTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AKTableView
+//
+//  Created by Avinash kiran on 18/06/18.
+//  Copyright © 2018 Avinash kiran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
