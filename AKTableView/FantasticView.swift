ca8d1e8810e00b3b152712522162472ea3594c78
Avinash <avinashkiran001@gmail.com>
2018-06-18 23:17:54 +0530

    init

---
+++ AKTableView/FantasticView.swift
@@ -0,0 +1,21 @@
+//
+//  FantasticView.swift
+//  AKTableView
+//
+//  Created by Avinash kiran on 18/06/18.
+//  Copyright © 2018 Avinash kiran. All rights reserved.
+//
+
+import UIKit
+
+class FantasticView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
